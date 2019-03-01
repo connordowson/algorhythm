644c32837564dfdbee2487fad49fd6b716d81fc0
connordowson <connormwdowson@gmail.com>
2019-03-01 13:05:42 +0000

    Buttons to select time range now work

--- algorhythm/recommend/views.py
+++ algorhythm/recommend/views.py
@@ -17,49 +17,12 @@ oauth = util.prompt_for_user_token(username, scope, client_id, client_secret, re
 
 # Create your views here.
 
-def get_top_tracks(time_range):
-    if oauth:
-        spotify = spotipy.Spotify(auth=oauth)
-        results = spotify.current_user_top_tracks(limit=50, offset=0, time_range=time_range)
-
-        tracks_list = []
-        audio_features = []
-
-        top_tracks = results['items']
-
-        for index, track in enumerate(results['items']):
-            track_info = {}
-            track_info['song_id'] = track['id']
-            track_info['title'] = track['name']
-            track_info['artist'] = track['artists'][0]['name']
-            track_info['album'] = track['album']['name']
-            track_info['image_url'] = track['album']['images'][0]['url']
-
-            release_date_precision = track['album']['release_date_precision']
-
-            if(release_date_precision == "year"):
-                track_info['release_date'] = int(track['album']['release_date'])
-            else:               
-                formatted_date = datetime.strptime(track['album']['release_date'], '%Y-%m-%d')
-                year = formatted_date.year
-                track_info['release_date'] = int(year)
-
-            # Get audio features for users top songs
-            this_audio_features = {}
-            results = spotify.audio_features(track['id'])
-            track_info['audio_features'] = results[0]
-
-            # Add list of tracks to variable to be used in context
-            tracks_list.append(track_info)
-            
-    return tracks_list
-
 @login_required
 def recommend(request):
    
     context = {
         'current_user': request.user.first_name,
-        'top_tracks': 'empty'
+        # 'top_tracks': 'empty'
     }
 
     if(request.GET.get('long_term')):
@@ -75,7 +38,78 @@ def recommend(request):
 
     # upload_songs(request.user.id, get_top_tracks('long_term'), 'Long term')
 
-    return render(request, 'recommend/recommend.html', context=context)
+    return render(request, 'recommend/recommend.html', context = context)
+
+@login_required
+def top_tracks(request):
+
+    if request.method == 'GET':
+
+        time_range = request.GET.get('time_range')
+        top_tracks = get_top_tracks(time_range)        
+
+        context = {
+            'top_tracks': get_top_tracks(time_range),
+            'time_range': time_range
+        }
+
+        upload_songs(request.user.id, top_tracks, time_range)
+
+        return render(request, 'recommend/top_tracks.html', context = context)
+
+
+    return render(request, 'recommend/top_tracks.html', context = context)
+
+def get_top_tracks(time_range):
+
+    if oauth:
+        spotify = spotipy.Spotify(auth=oauth)
+        results = spotify.current_user_top_tracks(limit=25, offset=0, time_range=time_range)
+
+        tracks_list = []
+        audio_features = []
+
+        top_tracks = results['items']
+
+        for index, track in enumerate(top_tracks):
+
+            # Check if song info is already on the database
+            if not Song.objects.filter(song_id = track['id']).exists():
+
+                track_info = {}
+                track_info['song_id'] = track['id']
+                track_info['title'] = track['name']
+                track_info['artist'] = track['artists'][0]['name']
+                track_info['album'] = track['album']['name']
+                track_info['image_url'] = track['album']['images'][0]['url']
+
+                release_date_precision = track['album']['release_date_precision']
+
+                if(release_date_precision == "year"):
+                    track_info['release_date'] = int(track['album']['release_date'])
+                else:               
+                    formatted_date = datetime.strptime(track['album']['release_date'], '%Y-%m-%d')
+                    year = formatted_date.year
+                    track_info['release_date'] = int(year)
+
+                # Get audio features for users top songs
+                results = spotify.audio_features(track['id'])
+                track_info['audio_features'] = results[0]
+
+                # Add list of tracks to variable to be used in context
+                tracks_list.append(track_info)
+            else:
+                this_song = Song.objects.get(song_id = track['id'])
+                track_info = {}
+                track_info['song_id'] = getattr(this_song, 'song_id')
+                track_info['title'] = getattr(this_song, 'title')
+                track_info['artist'] = getattr(this_song, 'artist')
+                track_info['album'] = getattr(this_song, 'album')
+                tracks_list.append(track_info)
+
+
+            
+    return tracks_list
 
 def upload_songs(user_id, songs, time_range):
 
@@ -118,6 +152,6 @@ def upload_songs(user_id, songs, time_range):
             this_top_tracks.save()
 
 
-
+@login_required
 def index(request):
     return render(request, 'recommend/index.html')
