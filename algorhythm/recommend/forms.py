6603a2a05f198a853900464a812682e90bae95bd
Connor Dowson <connormwdowson@gmail.com>
2019-04-22 14:15:08 +0100

    Added feedback

--- algorhythm/recommend/forms.py
+++ algorhythm/recommend/forms.py
@@ -1,3 +1,6 @@
 from django.contrib.auth.models import User
+from .models import Song, UserTopTracks, User, Recommendation
 from django import forms
 
+class FeedbackForm(forms.Form):
+    song_id = 
